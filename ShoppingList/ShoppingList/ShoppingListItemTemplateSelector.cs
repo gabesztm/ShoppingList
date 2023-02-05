c5b9da6965895247572c8ee943b24719b9c3d789
gabesztm <gabesz.tm@gmail.com>
2023-02-05 15:06:35 +0100

    Add itemSelector as Frame has bugs on Windows https://github.com/dotnet/maui/issues/5600

---
+++ ShoppingList/ShoppingList/ShoppingListItemTemplateSelector.cs
@@ -0,0 +1,16 @@
+﻿namespace ShoppingList
+{
+    internal class ShoppingListItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate Template { get; set; }
+        public DataTemplate WindowsTemplate { get; set; }
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if(DeviceInfo.Current.Platform == DevicePlatform.WinUI)
+            {
+                return WindowsTemplate;
+            }
+            return Template;
+        }
+    }
+}
